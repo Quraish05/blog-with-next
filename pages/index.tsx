36d1a0bdd7b720af31090e8e3641e76bbedcc4ca
Quraish05 <quraishnagri@gmail.com>
2023-05-10 19:50:45 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import { useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  let abc: number;
+
+  interface DemoObj {
+    phoneNumber?: number;
+  }
+
+  useEffect(() => {
+    console.log(4 / abc);
+
+    return () => {};
+  }, []);
+
   return (
     <>
       <Head>
@@ -26,7 +39,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +132,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
